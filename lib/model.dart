4036c8b040291304f9c31659f6ff0977cefc5f5e
kuvaan16 <kovannerway41@gmail.com>
2023-08-20 15:15:50 +0300

    day34

---
+++ lib/model.dart
@@ -0,0 +1,219 @@
+class ClothesData {
+  String? cover;
+  String? name;
+  String? desc;
+  String? value;
+  int? rait;
+  bool? isliked;
+  ClothesData(
+    this.cover,
+    this.name,
+    this.desc,
+    this.value,
+    this.rait,
+    this.isliked,
+  );
+}
+
+List<ClothesData> allData = [
+  ClothesData(
+    "img/first.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$212.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/w3.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$57.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/w4.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$20.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat1.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$50.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat2.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/w1.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$50.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/w2.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/second.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/third.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$250.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/fourth.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$200.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat3.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$57.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat4.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$20.99",
+    5,
+    false,
+  ),
+];
+List<ClothesData> clothesData = [
+  ClothesData(
+    "img/first.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$212.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/second.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/third.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$250.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/fourth.jpg",
+    "Modern light clothes",
+    "Dress modern",
+    "\$200.99",
+    5,
+    false,
+  ),
+];
+
+List<ClothesData> hatData = [
+  ClothesData(
+    "img/hat1.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$50.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat2.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/hat3.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$57.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/hat4.jpg",
+    "Jordan Jumpman Washed Bucket Hat",
+    "Hat modern",
+    "\$20.99",
+    5,
+    false,
+  ),
+];
+
+List<ClothesData> watcheData = [
+  ClothesData(
+    "img/w1.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$50.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/w2.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$116.99",
+    5,
+    true,
+  ),
+  ClothesData(
+    "img/w3.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$57.99",
+    5,
+    false,
+  ),
+  ClothesData(
+    "img/w4.jpg",
+    "Black Citizen Analog Watch",
+    "Watche modern",
+    "\$20.99",
+    5,
+    false,
+  ),
+];
